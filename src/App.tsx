e4fa0ec5a8a234bb779d7883a22158ee6b287718
DanSirias <danielsirias.88@gmail.com>
2023-06-25 22:29:29 -0400

    update 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test
+          Learn React Test 2
         </a>
       </header>
     </div>
