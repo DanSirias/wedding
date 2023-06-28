896fca5b116ed3009f3756d8bd771ed9d4103e22
DanSirias <danielsirias.88@gmail.com>
2023-06-27 21:17:42 -0400

    logos added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import logo from './images/RDlogo.svg'; 
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-
-        <img className='logo' src={logo} alt="Logo" />
-
+      <img className='logo' src={logo} alt="Logo" /> 
       </header>        <p>
           COMING SOON
         </p>
