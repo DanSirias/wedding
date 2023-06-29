341634d12a0872127e1abc5311066a5a55d553f2
DanSirias <danielsirias.88@gmail.com>
2023-06-29 01:59:38 -0400

    bug solved

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -99,7 +99,7 @@ function Navbar() {
               {pages.map((page) => (
                 <MenuItem key={page.text} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">
-                  <Link component={RouterLink} to={`/${page.href}`} style={{textDecoration: "None", color: "maroon"}} color="inherit" underline="none" sx={{ mx: 2 }}>{page.text}</Link>
+                  <Link component={RouterLink} to={`${page.href}`} style={{textDecoration: "None", color: "maroon"}} color="inherit" underline="none" sx={{ mx: 2 }}>{page.text}</Link>
                   </Typography>
                 </MenuItem>
               ))}
