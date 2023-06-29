fd4c5e2151f250033dc81e99b375e6492e25f577
DanSirias <danielsirias.88@gmail.com>
2023-06-29 08:48:53 -0400

    sections updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -139,7 +139,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Rebecca Sirias" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
