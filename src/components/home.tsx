687177dd98a3ce7a142ba82af1863e1f142c57e6
DanSirias <danielsirias.88@gmail.com>
2023-09-23 13:21:29 -0400

    home updated

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import "../App.css"
 import Logo from '../images/RDlogo_burgandy.svg'
 import LetterLogo from '../images/names.svg';
+import treeBackground from '../images/treePic.jpg';
+
 export const Home: React.FC = () => {
   return (<>
-  <div className="" style={{ padding: 30, height: "100%" }}>
+  <div className="home" style={{ padding: 30, height: "100%" }}>
   <div className="section">
   <img id="" className="logo" src={Logo}/>
   <img id="" className="namelogoPri" src={LetterLogo}/>
