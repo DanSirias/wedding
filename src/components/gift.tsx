f18d410b6651e5f7ff7718b7cef20accdc95bf74
DanSirias <danielsirias.88@gmail.com>
2023-09-24 01:43:44 -0400

    updated

--- src/components/gift.tsx
+++ src/components/gift.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import "../App.css"
 
 export const GiftRegistry: React.FC = () => {
-  return (<><h2 className="section">GiftRegistry</h2></>);
+  return (<>
+  <div className="registry" style={{ padding: 30, height: "100%"}}>
+  <h2 className="section">GiftRegistry</h2>
+  </div>
+  </>);
 };
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,6 +6,7 @@ import treeBackground from '../images/treePic.jpg';
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme({
@@ -43,6 +44,18 @@ export const Home: React.FC = () => {
     RVSP
   </Button>
 </Link>
+{/* <Typography id="reccsHome" component="h2" variant="h5"
+            sx={{
+              width: "80%",
+              justifyContent:"center",
+              alignItems:"center",
+            }}>
+             We find each other under this grand ol’ Texas Century Tree, 
+             which continues to bind the threads of love for over a century. 
+             And now, I wish for our story to intertwine with its legacy and tradition.
+
+            </Typography>
+ */}
   </div>
   </div></ThemeProvider>
   </>)
