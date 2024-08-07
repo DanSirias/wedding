3da89fe0f74726ea771fb6877dba28d160dd889d
DanSirias <danielsirias.88@gmail.com>
2024-08-07 19:07:15 -0400

    main updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import {Home} from './components/home';
 import {Events} from './components/events';
 import {Travel} from './components/travel';
