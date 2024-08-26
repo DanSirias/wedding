b739efb61b623155c5ae3ce884ff9fde5fa8d840
DanSirias <danielsirias.88@gmail.com>
2024-08-25 22:37:41 -0400

    nav update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -232,7 +232,7 @@ function Navbar() {
                 <LoginIcon sx={{ marginRight: "0px" }} />
                 <Link
                   component={RouterLink}
-                  to="/login"
+                  to="https://siriaswedding.auth.us-east-1.amazoncognito.com/login?client_id=5bfvpm9fvjgbh5c2s32oiv9d6h&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.siriaswedding.com%2Fdashboard"
                   style={{ textDecoration: "None", color: "#321115" }}
                   color="inherit"
                   underline="none"
